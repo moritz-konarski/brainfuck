c62b31bd1581d3e2d7b4a4038c01f495a83e1144
Moritz Konarski <moritz.konarski@gmail.com>
2020-07-27 20:25:49 +0600

    interpreter: add command support, syntax checking, bracket matching

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -8,29 +8,39 @@
  *  - '-' decrement the byte at the data pointer
  *  - '.' output the byte at the data pointer
  *  - ',' accept one byte of input, storing it at the data pointer
- *  - '[' if the byte at the pointer is 0, jump to ']', else to next command 
+ *  - '[' if the byte at the pointer is 0, jump to ']', else to next command
  *  - ']' if the data pointer is not 0, jump to '[', else to next command
- *  
+ *
  *  standard implementation has an array of 30,000 cells of u8
  *  what happens when the pointer goes out of bounds -- error? -- best solution
  */
-use std::io;
 use std::char;
+use std::io::{self, Write};
+
+const ARRAY_SIZE: usize = 30_000;
 
 struct MemorySpace {
     index: usize,
-    array: [u32; 30_000],
+    array: [u8; ARRAY_SIZE],
 }
 
-impl MemorySpace{
+impl MemorySpace {
     // action for '>'
     fn pointer_increment(&mut self) {
-        self.index += 1;
+        if self.index < ARRAY_SIZE - 1 {
+            self.index += 1;
+        } else {
+            self.index = 0;
+        }
     }
 
     // action for '<'
     fn pointer_decrement(&mut self) {
-        self.index -= 1;
+        if self.index > 1 {
+            self.index -= 1;
+        } else {
+            self.index = ARRAY_SIZE - 1;
+        }
     }
 
     // action for '+'
@@ -44,8 +54,8 @@ impl MemorySpace{
     }
 
     // action for '.'
-    fn output_data(&self) -> char {
-        match char::from_u32(self.array[self.index]) {
+    fn get_data(&self) -> char {
+        match char::from_u32(self.array[self.index] as u32) {
             Some(c) => c,
             None => ' ',
         }
@@ -53,20 +63,20 @@ impl MemorySpace{
 
     // action for ','
     fn write_data(&mut self, c: char) {
-        self.array[self.index] = c as u32;
+        self.array[self.index] = c as u8;
     }
 
     fn reset(self) -> MemorySpace {
         MemorySpace {
             index: 0,
-            array: [0; 30_000],
+            array: [0; ARRAY_SIZE],
         }
     }
 
     fn new() -> MemorySpace {
         MemorySpace {
             index: 0,
-            array: [0; 30_000],
+            array: [0; ARRAY_SIZE],
         }
     }
 
@@ -74,37 +84,137 @@ impl MemorySpace{
         self.index
     }
 
-    fn get_array(&self) -> &[u32] {
-        &self.array[..100]
+    fn get_array(&self) -> &[u8] {
+        &self.array[..25]
+    }
+
+    fn is_data_zero(&self) -> bool {
+        self.array[self.index] == 0
     }
 }
 
 fn main() {
     println!("Brainfuck Interpreter");
 
-    let mut input = String::new();
-    let mut mem_space = MemorySpace::new();
+    let mut input: String = String::new();
+    let mut mem_space: MemorySpace = MemorySpace::new();
+    let mut quit: bool = false;
+    let mut is_command: bool = false;
 
     loop {
-        io::stdin().read_line(&mut input)
-            .expect("Could not read input");
-        
-            // TODO: redo this using u8 because of many programs taking advantage of that
+        print!("bf> ");
+        match io::stdout().flush() {
+            Ok(_) => (),
+            Err(_) => {
+                eprintln!("Error: writing to screen failed");
+                break;
+            }
+        }
 
-        for c in input.chars() {
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error: could not read input");
+
+        input = input.trim().to_string();
+
+        if !check_line_syntax(&input) {
+            eprintln!("Error: brackets do not match");
+            break;
+        }
+
+        let input: Vec<_> = input.chars().collect();
+
+        let bracket_pairs = get_bracket_pairs(&input);
+        println!("{:?}", bracket_pairs);
+        let mut open_bracket_index = 0;
+        let mut closed_bracket_index = 0;
+        let mut i = 0;
+
+        loop {
+            if i >= input.len() {
+                break;
+            }
+
+            let c = input[i];
+            i += 1;
+
             match c {
                 '>' => mem_space.pointer_increment(),
                 '<' => mem_space.pointer_decrement(),
                 '+' => mem_space.data_increment(),
                 '-' => mem_space.data_decrement(),
-                '.' => println!("{}", mem_space.output_data()),
-                ',' => mem_space.write_data(c),
+                '.' => println!("{}", mem_space.get_data()),
+                // TODO: make input function
+                ',' => mem_space.write_data('c'),
+                /*
+                '[' => {
+                    if mem_space.is_data_zero() {
+                        i = bracket_pairs[open_bracket_index].1;                                              
+                        open_bracket_index += 1;
+                    }
+                },
+                ']' => {
+                    if !mem_space.is_data_zero() {
+                        i = bracket_pairs[closed_bracket_index].0;                                              
+                        closed_bracket_index += 1;
+                    }
+                },
+                */
+                ':' => {is_command = true; continue;},
                 _ => (),
-            }
+            };
+
+            if is_command {
+                is_command = false;
+                match c {
+                'r' => mem_space = mem_space.reset(),
+                'q' => {
+                    mem_space = mem_space.reset();
+                    quit = true;
+                }
+                _ => (),
+                };
+            };
         }
-        
-        println!("  {}", input);
+
         println!("{:#?}; {:?}", mem_space.get_index(), mem_space.get_array());
+        if quit {
+            break;
+        }
     }
 }
 
+fn check_line_syntax(input: &String) -> bool {
+    let mut open_bracket_counter: u16 = 0;
+    let mut closed_bracket_counter: u16 = 0;
+    for c in input.chars() {
+        match c {
+            '[' => open_bracket_counter += 1,
+            ']' => closed_bracket_counter += 1,
+            _ => (),
+        }
+    }
+    open_bracket_counter == closed_bracket_counter
+}
+
+fn get_bracket_pairs(input_vec: &Vec<char>) -> Vec<(usize, usize)> {
+    let mut pair_vec: Vec<(usize, usize)> = Vec::new();
+    let mut bracket_order: Vec<usize> = Vec::new();
+
+    for i in 0..input_vec.len() {
+        let c = input_vec[i];
+        //println!("{:?}", pair_vec);
+        match c {
+            '[' => {
+                pair_vec.push((i, 0));
+                bracket_order.push(pair_vec.len()-1);
+            },
+            ']' => {
+                pair_vec[bracket_order[bracket_order.len()-1]].1 = i;
+                bracket_order.remove(bracket_order.len() - 1);
+            },
+            _ => (),
+        }        
+    }
+    pair_vec
+}
