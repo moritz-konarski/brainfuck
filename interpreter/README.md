7b47b7ec1cf951e8780e595905f5c6ba996088e6
Moritz Konarski <moritz.konarski@gmail.com>
2020-08-17 20:45:51 +0600

    finish parser for commands

--- interpreter/README.md
+++
@@ -1,4 +0,0 @@
-# Brainfuck Interpreter
-
-Interpreter for the brainfuck programming language. It has 30,000 cells of
-memory of unsigned 32-bit integers and index bounds checks.
--- interpreter/src/parser.rs
+++ interpreter/src/parser.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 /// enum of all 8 brainfuck commands
 pub enum BrainfuckCommand {
     PointerIncrement,
@@ -25,6 +27,7 @@ pub enum Command {
 
 /// parser for brainfuck commands
 pub struct Parser {
+    commands: Vec<Command>,
     bracket_pairs: Vec<(usize, usize)>,
 }
 
@@ -32,29 +35,84 @@ impl Parser {
     /// returns a new instance of parser
     pub fn new() -> Self {
         Parser {
+            commands: Vec::new(),
             bracket_pairs: Vec::new(),
         }
     }
 
-    /// function to parse a string and turn it into a vector of commands
-    pub fn parse_string(&mut self, input_string: &String) -> Option<Vec<Command>> {
-        let mut next_is_command = false;
-        let mut parsed_commands: Vec<Command> = Vec::new();
+    pub fn from_string(input_string: &String) -> Option<Self> {
+        match Self::parse_commands(input_string) {
+            Some(commands) => {
+                // TODO: parse brackets
+                return Some(Self{commands, bracket_pairs: Vec::new()})
+            },
+            None => return None,
+        }
+    }
 
-        for c in input_string.chars() {
+    /// function to parse a string and turn it into a vector of commands
+    pub fn parse_string(&mut self, input_string: &String) -> io::Result<bool> {
+        match Self::parse_commands(input_string) {
+            Some(commands) => {
+                self.commands = commands;
+                // TODO: parse brackets
+                return Ok(true)},
+            // returns a system io error in linux
+            None => Err(io::Error::from_raw_os_error(5)),
+        }
+    }
+
+    fn create_bracket_pairs(commands: &Vec<Command>) -> Vec<(usize, usize)> {
+        let mut pair_vec: Vec<(usize, usize)> = Vec::new();
+        let mut bracket_order: Vec<usize> = Vec::new();
+
+        // TODO: fix
+        for i in 0..input_vec.len() {
+            let c = input_vec[i];
             match c {
-                '>' => parsed_commands.push(Command::Brainfuck(BrainfuckCommand::PointerIncrement)),
-                '<' => parsed_commands.push(Command::Brainfuck(BrainfuckCommand::PointerDecrement)),
-                '+' => parsed_commands.push(Command::Brainfuck(BrainfuckCommand::DataIncrement)),
-                '-' => parsed_commands.push(Command::Brainfuck(BrainfuckCommand::DataDecrement)),
+                '[' => {
+                    pair_vec.push((i, 0));
+                    bracket_order.push(pair_vec.len() - 1);
+                }
+                ']' => {
+                    pair_vec[bracket_order[bracket_order.len() - 1]].1 = i;
+                    bracket_order.remove(bracket_order.len() - 1);
+                }
+                _ => (),
+            }
+        }
+        pair_vec
+    }
+
+    /// function that turns a string into a vector of commands
+    fn parse_commands(string: &String) -> Option<Vec<Command>> {
+
+        let mut next_is_command = false;
+        let mut commands: Vec<Command> = Vec::new();
+
+        let mut open_bracket_counter = 0;
+        let mut closed_bracket_counter = 0;
+
+        for c in string.chars() {
+            match c {
+                '>' => commands.push(Command::Brainfuck(BrainfuckCommand::PointerIncrement)),
+                '<' => commands.push(Command::Brainfuck(BrainfuckCommand::PointerDecrement)),
+                '+' => commands.push(Command::Brainfuck(BrainfuckCommand::DataIncrement)),
+                '-' => commands.push(Command::Brainfuck(BrainfuckCommand::DataDecrement)),
                 '.' => {
-                    parsed_commands.push(Command::Brainfuck(BrainfuckCommand::ReturnDataAtPointer))
+                    commands.push(Command::Brainfuck(BrainfuckCommand::ReturnDataAtPointer))
                 }
                 ',' => {
-                    parsed_commands.push(Command::Brainfuck(BrainfuckCommand::WriteDataToPointer))
+                    commands.push(Command::Brainfuck(BrainfuckCommand::WriteDataToPointer))
                 }
-                '[' => parsed_commands.push(Command::Brainfuck(BrainfuckCommand::OpenBracket)),
-                ']' => parsed_commands.push(Command::Brainfuck(BrainfuckCommand::ClosedBracket)),
+                '[' => {
+                    commands.push(Command::Brainfuck(BrainfuckCommand::OpenBracket));
+                    open_bracket_counter += 1;
+                },
+                ']' => { 
+                    commands.push(Command::Brainfuck(BrainfuckCommand::ClosedBracket));
+                    closed_bracket_counter += 1;
+                },
                 ':' => {
                     next_is_command = true;
                     continue;
@@ -65,35 +123,18 @@ impl Parser {
             if next_is_command {
                 next_is_command = false;
                 match c {
-                    'r' => parsed_commands.push(Command::Shell(ShellCommand::ResetValues)),
-                    'p' => parsed_commands.push(Command::Shell(ShellCommand::PrintString)),
-                    'q' => parsed_commands.push(Command::Shell(ShellCommand::QuitProgram)),
+                    'r' => commands.push(Command::Shell(ShellCommand::ResetValues)),
+                    'p' => commands.push(Command::Shell(ShellCommand::PrintString)),
+                    'q' => commands.push(Command::Shell(ShellCommand::QuitProgram)),
                     _ => (),
                 };
             }
         }
 
-        //if !check_matching_brackets(&parsed_commands) {
-        //    return None;
-        //}
-
-        Some(parsed_commands)
-    }
-
-    /// check the commands for matching numbers of strings
-    fn check_matching_brackets(commands: &Vec<Command>) -> bool {
-        let mut open_bracket_counter = 0;
-        let mut closed_bracket_counter = 0;
-        for c in commands {
-            match c {
-                Command::Brainfuck(x) => match x {
-                    BrainfuckCommand::OpenBracket => open_bracket_counter += 1,
-                    BrainfuckCommand::ClosedBracket => closed_bracket_counter += 1,
-                    _ => (),
-                },
-                _ => (),
-            };
+        if open_bracket_counter != closed_bracket_counter {
+            None
+        } else {
+            Some(commands)
         }
-        open_bracket_counter == closed_bracket_counter
     }
 }
